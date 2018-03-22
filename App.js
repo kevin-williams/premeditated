7d9f3d8d65880efbb608617c1964ebca81ef8a9b
Kevin Williams <kwilliams@discounttire.com>
2018-03-22 09:21:29 -0700

    Added Font Loader class to load custom icons

--- App.js
+++ App.js
@@ -3,42 +3,24 @@ import { View } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import ReduxThunk from 'redux-thunk';
-import { AppLoading, Font } from 'expo';
+import { AppFontLoader } from './src/utils';
 
 import reducers from './src/reducers';
-import FontAwesome from './node_modules/@expo/vector-icons/fonts/FontAwesome.ttf';
-import MaterialIcons from './node_modules/@expo/vector-icons/fonts/MaterialIcons.ttf';
 
 import TimerList from './src/containers/timer/TimerList';
 
 export default class App extends React.Component {
-  state = {
-    fontLoaded: false
-  };
-
-  async componentWillMount() {
-    try {
-      await Font.loadAsync({
-        FontAwesome,
-        MaterialIcons
-      });
-      this.setState({ fontLoaded: true });
-    } catch (error) {
-      console.log('error loading icon fonts', error);
-    }
-  }
-
   render() {
-    if (!this.state.fontLoaded) {
-      return <AppLoading />;
-    }
-
     return (
-      <Provider store={createStore(reducers, {}, applyMiddleware(ReduxThunk))}>
-        <View style={styles.container}>
-          <TimerList />
-        </View>
-      </Provider>
+      <AppFontLoader>
+        <Provider
+          store={createStore(reducers, {}, applyMiddleware(ReduxThunk))}
+        >
+          <View style={styles.container}>
+            <TimerList />
+          </View>
+        </Provider>
+      </AppFontLoader>
     );
   }
 }
