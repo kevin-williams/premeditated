05fb2fa56624e9399a978be376c5c97dd2d10c03
Kevin Williams <kwilliams@discounttire.com>
2018-03-22 08:45:51 -0700

    Added reqs to readme

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # premeditated
+
 Meditation timer app
+
+I just want to replicate the meditation timers… the content is actually bullshit and would be easiest once you get the thing rolling.
+
+    Free app with ad revenue would be best to start
+    Content for free + in-app purchases would be great as later stage item
+
+The real value is being able to create timers that have bells at either specific intervals or set times and making them easy to create.
+
+    Hate the fact that you have to explicitly set the time of the bell determined as a fixed amount of time from the start or end… you can’t just add 3 mins from last bell which would make this much more intuitive
+    Love the fact that you can create and save timers with individual names etc
+        ***being able to publish and share these timers on social media would be awesome so that way you could drive additional adoption quickly***
+        one yoga teacher with a youtube channel publishing a web version of this around a specific series or something - which drove users to download app – would drive adoption like crazy, man
+
+The social platform is bullshit… would be way better to just connect to FB and/or InstaGram and youtube – again timers should be shareable once created in the app
+Like the fact that you can play some background/ambient noise during timers (I can acquire this content no problem)
+
+    Revenue oppty would be to drive in-app purchases for additional background noise
---
+++ src/containers/timer/AddEditTimer.js
@@ -0,0 +1,138 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Text, TextInput, TouchableHighlight, View } from 'react-native';
+import { Button } from 'react-native-elements';
+
+import TimeSelect from '../../components/TimeSelect';
+
+import { addTimer } from './timerActions';
+
+import { SCREEN_WIDTH } from '../../utils';
+
+const NEW_TIMER = {
+  title: '',
+  selectedHours: 0,
+  selectedMinutes: 10,
+  intervalHours: 0,
+  intervalMinutes: 5,
+  timerId: undefined,
+  intervalId: undefined
+};
+
+class AddEditTimer extends Component {
+  state = {
+    ...NEW_TIMER
+  };
+
+  saveTimer() {
+    this.props.addTimer(this.state);
+    this.props.closeModal();
+  }
+
+  handleClose() {
+    this.props.closeModal();
+  }
+
+  render() {
+    const {
+      selectedHours,
+      selectedMinutes,
+      intervalHours,
+      intervalMinutes
+    } = this.state;
+
+    return (
+      <View style={styles.container}>
+        <View style={styles.closeContainer}>
+          <Text style={styles.headerText}>Add/Edit Timer</Text>
+          <TouchableHighlight
+            style={styles.closeButton}
+            underlayColor="#777"
+            onPress={this.handleClose.bind(this)}
+          >
+            <Text style={{ fontSize: 18 }}>X</Text>
+          </TouchableHighlight>
+        </View>
+        <View style={styles.bottom}>
+          <Text>Name</Text>
+          <TextInput
+            style={{ width: 200, height: 40 }}
+            onChangeText={text => this.setState({ title: text })}
+            placeholder="timer name"
+            value={this.state.title}
+          />
+
+          <TimeSelect
+            label="Duration"
+            hours={selectedHours}
+            minutes={selectedMinutes}
+            onTimeSelected={time => {
+              console.log('change time=', time);
+              this.setState({
+                selectedHours: time.hours,
+                selectedMinutes: time.minutes
+              });
+            }}
+          />
+
+          <TimeSelect
+            label="Interval"
+            hours={intervalHours}
+            minutes={intervalMinutes}
+            onTimeSelected={time => {
+              console.log('change interval=', time);
+              this.setState({
+                intervalHours: time.hours,
+                intervalMinutes: time.minutes
+              });
+            }}
+          />
+
+          <Button
+            large
+            title="Save"
+            onPress={this.saveTimer.bind(this)}
+            backgroundColor="green"
+          />
+        </View>
+      </View>
+    );
+  }
+}
+
+const styles = {
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  bottom: {
+    flex: 3,
+    marginTop: 20
+  },
+  headerText: {
+    textAlign: 'center',
+    fontSize: 18,
+    flex: 5
+  },
+  closeContainer: {
+    width: SCREEN_WIDTH,
+    borderBottomWidth: 0.5,
+    paddingBottom: 10,
+    backgroundColor: '#F9F9F9',
+    flexDirection: 'row',
+    alignItems: 'flex-end'
+  },
+  closeButton: {
+    width: 40,
+    height: 40,
+    flex: 1,
+    alignSelf: 'flex-end',
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+};
+
+const mapStateToProps = state => state;
+export default connect(mapStateToProps, { addTimer })(AddEditTimer);
