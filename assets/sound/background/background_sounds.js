9a469f003f86447471397c28a9fe09e37a2861be
Kevin Williams <kwilliams@discounttire.com>
2018-04-09 08:57:35 -0700

    Replace background mp3s with wavs for higher quality

--- assets/sound/background/background_sounds.js
+++ assets/sound/background/background_sounds.js
@@ -6,32 +6,32 @@ export const backgroundSounds = [
   },
   {
     name: 'Liquid',
-    file: require('./Liquid.mp3'),
+    file: require('./Liquid.wav'),
     license: 'Recorded by Caroline Ford'
   },
   {
     name: 'River',
-    file: require('./River.mp3'),
+    file: require('./River.wav'),
     license: 'Recorded by Mike Koenig'
   },
   {
     name: 'Stream',
-    file: require('./Stream.mp3'),
+    file: require('./Stream.wav'),
     license: 'Recorded by Caroline Ford'
   },
   {
     name: 'Tree Frogs',
-    file: require('./Tree_Frogs.mp3'),
+    file: require('./Tree_Frogs.wav'),
     license: 'Recorded by Mike Koenig'
   },
   {
     name: 'Waterfall',
-    file: require('./Waterfall.mp3'),
+    file: require('./Waterfall.wav'),
     license: 'Recorded by Mike Koenig'
   },
   {
     name: 'Wind',
-    file: require('./Wind.mp3'),
+    file: require('./Wind.wav'),
     license: 'Recorded by Mark DiAngelo'
   }
 ];
